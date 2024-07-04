7ced54b379db05bc590dbe71f77a98e4c680631e
Mks1311 <manish31311@gmail.com>
2024-07-04 12:16:42 +0530

    first commit

---
+++ sveltekit-demo/src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Navbar from "../Navbar.svelte";
+    import Footer from "../Footer.svelte";
+    import "../app.css";
+</script>
+<Navbar/>
+<slot/>
+<Footer/>
---
+++ sveltekit-demo/src/routes/blog/+page.svelte
@@ -0,0 +1,1 @@
+<h1 class="underline">THis is blog</h1>
